43a149578f7a71c3740dd15018fe535715580374
kwakminoo <kwakminoo@github.com>
2025-09-12 23:27:27 +0900

    Chapter2.py 업데이트: 문자열 포맷팅, 리스트, 튜플 예제 추가

--- Chapter2.py
+++ Chapter2.py
@@ -15,9 +15,70 @@ print(b)
 c = "i eat {0} apples".format(3)
 print(c)
 
-d = f"{one}까진 {t} {th}".format(one = 1, t = "까진", th = "할만한데?")
+d = "{one}까진 {t} {th}".format(one = 1, t = "까진", th = "할만한데?")
 print(d)
 
 # 문자열 슬라이싱 예제
 e = "나는 생각한다, 고로 존재한다."
-print(e[0:0:1])
+print(e[0:0:-1])
+ 
+# {:>10}10칸을 만들고 그 칸 안에서 오른쪽 정렬
+# {:<5}5칸을 만들고 그 칸 안에서 왼쪽 정렬
+# {:^7}7칸을 만들고 그 칸 안에서 가운데 정렬
+# .format()은 ()안에 있는 것을 대입한다
+e = "{0:^10}".format("hi")
+print(e)
+
+name = "곽민우"
+age = 23
+# f''는 format의 최신문법으로 format과 같은 기능을 한다.
+f = f'나의 이름은 {name}이고 나이는 {age}살이야.'
+print(f)
+ 
+g = "hobby"
+#count는 숫자를 새는 것이고 밑은 b의 개수를 새는 것이다.
+print(g.count('b'))
+
+# 리스트 안에 또 다른 리스트를 만들 수 있다.
+odd = [1,2,3,4,5, ['one', 'two', 'three', 'four','five']]
+#type은 말 그대로 그것의 클래스 타입을 나타낸다.
+print(type(odd))
+# 리스트 안에 리스트의 값을 출력할 때는 몇번 리스트의 몇번인지 동일하게 연속해서 사용하면 된다.
+print(odd[5][3])
+
+a = [1,2,3,4,5]
+# 밑은 a리스트의 [2]번 값을 변경하는 것이다.
+a[2] = "fuck"
+print(a)
+# del은 그 부분을 삭제한다.
+del a[2]
+# len은 길이를 새는 것으로 안에 변수가 5개라면 5를 출력한다다
+print(len(a))
+# sort는 오름차순 정렬
+a.sort()
+print(a)
+# reverse는 내림차순 정렬이다.
+a.reverse()
+print(a)
+
+test = ["angly", "gold", "xe", "hi", "under"]
+test.sort()
+print(test)
+test.reverse()
+print(test)
+
+a.insert(2, "fuck")
+print(a)
+a.pop(2)
+print(a)
+
+#  Mutable: 리스트, 딕셔너리, 집합 등 변경이 가능한 것.
+# Immutable: 정수, 실수, 문자열, 튜플 등 변경이 불가능 한 것.
+# 리스트: [] 튜플: ()
+
+t = 1,2,3
+print(t)
+t1 = (1, 3)
+print(t1[-1])
+t2 = (1, 2, (3, 4, 5))
+print(t2[1], t2[2][1])
